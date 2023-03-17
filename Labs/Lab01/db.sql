a69b959c052a8522a826a0ef6637e1b17cec48b0
snglrtycrvtureofspce <asp1rantzenevich@gmail.com>
2023-03-17 23:24:54 +0300

    #

---
+++ Labs/Lab01/db.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE wholesale_sales;
+
+USE wholesale_sales;
+
+CREATE TABLE products (
+  product_id INT PRIMARY KEY,
+  product_name VARCHAR(50) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  description VARCHAR(255),
+  delivery_available BOOLEAN NOT NULL,
+  delivery_options VARCHAR(255)
+);
+
+CREATE TABLE customers (
+  customer_id INT PRIMARY KEY,
+  customer_name VARCHAR(50) NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  phone VARCHAR(20) NOT NULL,
+  contact_person VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE orders (
+  order_id INT PRIMARY KEY,
+  product_id INT NOT NULL,
+  customer_id INT NOT NULL,
+  quantity INT NOT NULL,
+  order_date DATE NOT NULL,
+  delivery_type VARCHAR(50),
+  delivery_cost DECIMAL(10, 2),
+  FOREIGN KEY (product_id) REFERENCES products(product_id),
+  FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
